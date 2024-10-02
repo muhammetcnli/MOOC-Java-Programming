d47990ba344db65644d9525f978ff178c58a8fa5
muhammetcnli <howardroark756@gmail.com>
2024-10-02 20:35:09 +0300

    initial commit

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,45 +3,55 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+              
         for (int i = 1; i <= number; i++) {
             System.out.print("*");
-        }
-    }
+        }  
+        
+        
+    }   
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
         for (int i = 1; i <= number; i++) {
             System.out.print(" ");
         }
-        
 
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
-        for (int i = 1; i <= size; i++) {
-            printSpaces(size-i);
-            printStars(i);
-            System.out.println("");
-            
-            
+        if (size > 0) {
+            for (int i = 1; i <= size; i++) {
+                printSpaces(size - i);
+                printStars(i);
+                System.out.println("");
+
+            }
         }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
-        printTriangle(height);
-        
+        // width will be 2*height - 1
+        int spaces = height - 1;
+        for(int stars = 1; stars<= 2*height -1; stars = stars + 2){
+            printSpaces(spaces);
+            printStars(stars);
+            
+            spaces = spaces -1;
+            System.out.println("");
+        }
         printSpaces(height-2);
-        printStars(3);
-        System.out.println("");
+        System.out.println("***");
         printSpaces(height-2);
-        printStars(3);
+        System.out.println("***");
+
     }
 
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
-
+        
         printTriangle(5);
         System.out.println("---");
         christmasTree(4);
