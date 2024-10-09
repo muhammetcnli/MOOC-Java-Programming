af88d47b1bbca3aad83bda3b2267d984e8aa9cbb
muhammetcnli <howardroark756@gmail.com>
2024-10-09 14:31:41 +0300

    arrangements

---
+++ part4/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+
+        int count = 0;
+
+        try ( Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                int row = Integer.valueOf(fileScanner.nextLine());
+                if (row <= upperBound && row >= lowerBound) {
+                    count++;
+                }
+            }
+            
+            System.out.println("Numbers: " + count);
+        } catch (Exception e) {
+            System.out.println("Error reading file." + e.getMessage());
+        }
+    }
+
+}
